37bcb6c5c77b2e59d00109190cab2bb827a127af
lisamc <lisamccormack85@gmail.com>
2019-10-17 12:46:28 +0100

    prints any tasks to be completed at end of program

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,26 +1,5 @@
 import DOD from './app'
 
-
-/*
-it('prompts the user for an answer', async () => {
-    const prompt = jest.fn().mockResolvedValue('meow')
-    const consolelog = jest.fn()
-    //
-
-    await DOD(prompt, consolelog)
-
-    //
-    expect(prompt).toBeCalledWith('Tell me to shout something')
-    expect(consolelog).toBeCalledWith('MEOW')
-})*/
-
-// it('asks the first question in the questions array', async () => {
-//     const prompt = jest.fn()
-//     const consolelog = jest.fn()
-//     await DOD(prompt, consolelog)
-//     expect(prompt).toBeCalledWith("It is live and can be used (y/n) ")
-// })
-
 it('when user answers y it asks the next question', async () => {
     const prompt = jest.fn().mockResolvedValue('y')
     const consolelog = jest.fn()
@@ -47,5 +26,14 @@ it('says all checks passed if user says y', async() => {
     const questions = ['how are you?', 'will this work?', "how's the weather today?"]
     await DOD(prompt, consolelog, questions)
     expect(consolelog).toBeCalledWith("All checks passed")
+})
+
+it('prints out any tasks to complete at the end of the program', async() => {
+    const prompt = jest.fn().mockResolvedValue('n')
+    const consolelog = jest.fn()
+    const questions = ['how are you? ', 'will this work? ', "how's the weather today? "]
+    const notComplete:string[] = []
+    await DOD(prompt, consolelog, questions, notComplete)
+    expect(consolelog).toBeCalledWith(`Tasks yet to be completed: ${notComplete}`)
 })
 
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import { prompt } from "./cli";
 
-const colors = require("colors");
-
 type Prompter = typeof prompt;
 
 type Logger = typeof console.log;
@@ -12,20 +10,22 @@ export default async function(
   questions: string[],
   notComplete: string[] = []
 ) {
-  for (let i = 0; i < questions.length; ) {
+  for (let i = 0; i < questions.length; i++ ) {
     const answer = await p(questions[i]);
     if (answer === "y") {
       if (i === questions.length - 1) {
         logger("All checks passed");
-        i++
-      } else {
-        i++;
       }
     }
     if (answer === "n") {
       notComplete.push(questions[i]);
-      i++;
     }
+    if (i === questions.length - 1) {
+      if (notComplete.length > 0) {
+        logger(`Tasks yet to be completed: ${notComplete}`)
+      }
+    }
+
   }
 }
 
